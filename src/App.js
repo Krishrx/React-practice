9314ab02a4ac504ff252af20b4754e725c053faa
Krishrx <kalikrishnas342@gmail.com>
2023-09-10 23:34:52 +0530

    props basic

--- src/App.js
+++ src/App.js
@@ -3,10 +3,21 @@ function App() {
  
   return (
     <div className="App">
-
+      <User firstName='Kali' lastName='Krishna' country='Finland' />
       
     </div>
   );
+}
+
+const User = (props) => {
+  return (
+    <div>
+      <h1>
+        {props.firstName} {props.lastName}
+      </h1>
+      <small>{props.country}</small>
+    </div>
+  )
 }
 
 
