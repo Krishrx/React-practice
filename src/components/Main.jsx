6f1b6d829893c94f7bf4c44d75baf9a3fff54b45
Krishrx <kalikrishnas342@gmail.com>
2023-09-14 15:39:54 +0530

    completed

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,19 +4,35 @@ function Main() {
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
   const handleMouseOver = () => {
-    const newX = Math.random() * window.innerWidth;
-    const newY = Math.random() * window.innerHeight;
+    const newX = Math.random() * 500;
+    const newY = Math.random() * 500;
     setPosition({ x: newX, y: newY });
   };
 
+  const handleYes = () => {
+    alert('I know :)')
+  }
+
   return (
+    <div>
+      <div className='text-center p-5 font-medium'>
+        Are U an Idiot?
+      </div>
+      <div className='flex gap-4 justify-center text-center'>
+        <div className="rounded-lg w-[100px] p-2 bg-pink-700 text-white cursor-pointer"
+        onClick={handleYes}
+        >
+        Yes
+      </div>
         <div
-        className="random-div relative w-[100px] p-2 bg-pink-700 text-white cursor-pointer"
+        className="rounded-lg random-div relative w-[100px] p-2 bg-pink-700 text-white cursor-pointer"
         onMouseOver={handleMouseOver}
         style={{ left: position.x, top: position.y }}
         >
-        Hover me!
-        </div>
+        No
+      </div>
+      </div>
+    </div>
   );
 }
 
