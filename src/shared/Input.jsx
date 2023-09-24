9f186befbbc7d74467f86e4eff3c57635e9f6b6c
Krishrx <kalikrishnas342@gmail.com>
2023-09-24 23:14:54 +0530

    completed.

--- src/shared/Input.jsx
+++ src/shared/Input.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Input({type,name,placeholder,width}) {
   return (
-      <input type={type||'text'} name={name} placeholder={placeholder} className={`outline-orange-600 border border-orange-400 rounded-md p-3 w-[${width}%] `} />
+      <input type={type||'text'} style={{width:`${width}%`}} name={name} placeholder={placeholder} className={`outline-orange-600 border border-orange-400 rounded-md p-3`} />
   )
 }
 
