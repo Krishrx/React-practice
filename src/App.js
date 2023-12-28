550767bdc035d52f67cd29a72733d2b9a71539d9
Krishrx <kalikrishnas342@gmail.com>
2023-12-28 23:06:25 +0530

    need to add animation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Cards from './components/review_card/Cards'
-import Main from './components/sliding_image/Main';
+// import Main from './components/sliding_image/Main';
+import Main from './components/illusion_effect/Main';
 function App() {
  
   return (
     <>
-      <Cards/>
-      {/* <Main/> */}
+      {/* <Cards/> */}
+      <Main/>
     </>
   );
 }
---
+++ src/components/illusion_effect/Main.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <div className='w-full h-full bg-orange-400'>
+        <div className='w-full h-screen flex justify-center items-center px-2 gap-8'>
+            <img src="https://www.prokabaddi.com/static-assets/waf-images/6b/ec/db/16-9/oWEOozHmJS.jpg" alt="" className='basis-1/2 w-96 h-96 rounded-2xl object-cover'/>
+            <div className='basis-1/2 text-6xl font-semibold font-mono'>
+                Sultan Fazel Vs Rohit
+            </div>
+        </div>
+        <div className='w-full h-screen flex justify-center items-center px-2 gap-8'>
+            <img src="https://sc0.blr1.cdn.digitaloceanspaces.com/inline/qmuzncsgms-1670318674.jpg" alt="" className='basis-1/2 w-96 h-96 rounded-2xl object-cover'/>
+            <div className='basis-1/2 text-6xl font-semibold font-mono'>
+                Sultan Fazel's Solo Tackle
+            </div>
+        </div>
+        <div className='w-full h-screen flex justify-center items-center px-2 gap-8'>
+            <img src="https://khelkabaddi.in/wp-content/uploads/2022/10/IMG_20221021_214408.jpg" alt="" className='basis-1/2 w-96 h-96 rounded-2xl object-cover'/>
+            <div className='basis-1/2 text-6xl font-semibold font-mono'>
+                And it's a Wrap!
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Main
