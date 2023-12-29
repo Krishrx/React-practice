126aa08e5616619a598bc50192a38d34e3ea8ead
Krishrx <kalikrishnas342@gmail.com>
2023-12-29 12:30:34 +0530

    --

--- src/components/illusion_effect/Main.jsx
+++ src/components/illusion_effect/Main.jsx
@@ -1,27 +1,56 @@
 import React from 'react'
 
 function Main() {
-  return (
+return (
+<div className='w-full h-full bg-orange-400 font-mono'>
+    <div className='flex flex-col justify-center items-center p-4 w-full h-full'>
+        <h1 className='text-4xl font-medium py-4 uppercase'>Kabaddi</h1>
+        <div className='w-[80%] md:w-1/2 text-justify'>
+            <p className='font-serif'>Kabaddi is a contact team sport played between two teams of seven players, originating in ancient India.[2] The objective of the game is for a single player on offense, referred to as a "raider", to run into the opposing team's half of the court, touch out as many of their players as possible, and return to their own half of the court, all without being tackled by the defenders in 30 seconds. Points are scored for each player tagged by the raider, while the opposing team earns a point for stopping the raider. Players are taken out of the game if they are touched or tackled, but are brought back in for each point scored by their team from a tag or a tackle.
+
+            It is popular in the Indian subcontinent and other surrounding Asian countries. Although accounts of kabaddi appear in the histories of ancient India, the game was popularised as a competitive sport in the 20th century. It is the national sport of Bangladesh.[3] It is the second most popular and viewed sport in India after cricket.[2][4] It is the state game of the Indian states of Andhra Pradesh, Bihar, Chhattisgarh, Haryana, Karnataka, Kerala, Maharashtra, Odisha, Punjab, Tamil Nadu, Telangana, and Uttar Pradesh.[5]
+
+            There are two major disciplines: "Punjabi kabaddi", also called "circle style", comprises traditional forms of the sport that are played on a circular field outdoors, and the "standard style", on a rectangular court indoors, is played in major professional leagues and international competitions such as the Asian Games.
+
+            This game is known by numerous names in different parts of the Indian subcontinent, such as: kabaddi or chedugudu in Andhra Pradesh and Telangana; kabaddi in Maharashtra, Karnataka and Kerala; kabaddi, komonti or ha-du-du in West Bengal and Bangladesh; baibalaa in Maldives, kauddi or kabaddi in the Punjab region; hu-tu-tu in Western India, ha-do-do in Eastern India; chadakudu in South India; kapardi in Nepal; kabadi or sadugudu in Tamil Nadu; and chakgudu in Sri Lanka.[6]</p>
+        </div>
+    </div>
     <div className='w-full h-full bg-orange-400'>
         <div className='w-full h-screen flex justify-center items-center px-2 gap-8'>
             <img src="https://www.prokabaddi.com/static-assets/waf-images/6b/ec/db/16-9/oWEOozHmJS.jpg" alt="" className='basis-1/2 w-96 h-96 rounded-2xl object-cover'/>
-            <div className='basis-1/2 text-6xl font-semibold font-mono'>
+            <div className='basis-1/2 text-6xl font-semibold'>
                 Sultan Fazel Vs Rohit
             </div>
         </div>
         <div className='w-full h-screen flex justify-center items-center px-2 gap-8'>
             <img src="https://sc0.blr1.cdn.digitaloceanspaces.com/inline/qmuzncsgms-1670318674.jpg" alt="" className='basis-1/2 w-96 h-96 rounded-2xl object-cover'/>
-            <div className='basis-1/2 text-6xl font-semibold font-mono'>
+            <div className='basis-1/2 text-6xl font-semibold'>
                 Sultan Fazel's Solo Tackle
             </div>
         </div>
         <div className='w-full h-screen flex justify-center items-center px-2 gap-8'>
             <img src="https://khelkabaddi.in/wp-content/uploads/2022/10/IMG_20221021_214408.jpg" alt="" className='basis-1/2 w-96 h-96 rounded-2xl object-cover'/>
-            <div className='basis-1/2 text-6xl font-semibold font-mono'>
+            <div className='basis-1/2 text-6xl font-semibold'>
                 And it's a Wrap!
             </div>
         </div>
     </div>
+    <div className='flex flex-col justify-center items-center p-4 w-full h-full'>
+        <h1 className='text-4xl font-medium py-4 uppercase'>Conclusion</h1>
+        <div className='w-[80%] md:w-1/2 text-justify'>
+            <p className='font-serif'>Kabaddi, deeply rooted in the cultural fabric of India, is not merely a sport but a testament to the nation's resilience, teamwork, and agility. As we traverse the dynamic landscape of this exhilarating game, it becomes evident that kabaddi transcends its physical dimensions, embodying a spirit that resonates with the essence of unity.
+
+            In conclusion, kabaddi stands as a metaphor for life's challenges, emphasizing the significance of strategic planning, swift decision-making, and collaboration. The relentless back-and-forth nature of the game mirrors the constant ebb and flow of our daily struggles, urging individuals to confront adversity head-on.
+
+            Furthermore, kabaddi fosters a sense of community and mutual dependence. Success in the game hinges not solely on individual prowess but on the seamless coordination of the entire team. This echoes the societal value of collective progress, highlighting that achievements are often the result of collaborative efforts.
+
+            The sheer physicality of kabaddi teaches us the importance of staying grounded in the present moment. In a world that often propels us into the realms of the past or the future, kabaddi demands our undivided attention, promoting mindfulness and quick thinking—an invaluable skill set applicable to various facets of life.
+
+            As we celebrate the fervor and passion that kabaddi ignites, it becomes a microcosm of the larger journey we undertake. The cheers of the audience, the synchronized movements of players, and the thrill of the chase collectively weave a narrative of tenacity and triumph. In the end, kabaddi is more than a sport; it is a reflection of the indomitable spirit that propels us forward in our personal and collective pursuits.</p>
+        </div>
+    </div>
+        
+</div>
   )
 }
 
